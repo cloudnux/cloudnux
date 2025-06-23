eabb0f72317e29113300ea11ae830fe04cb7f78a
IGad <islam.gad@qliro.com>
2025-06-23 11:20:23 +0200

    fix: .env path in development

--- packages/cli/src/task-manager/tasks/dev-server-watch.ts
+++ packages/cli/src/task-manager/tasks/dev-server-watch.ts
@@ -29,12 +29,6 @@ export const devServerWatch: Task = {
             external: [...externalPackages],
             esbuildOptions: (options) => {
                 options.absWorkingDir = workingDir;
-                options.alias = {
-                    "@@cloudcore": "@cloudnux/cloud-core",
-                    "@@cloud": mapCloud(cloudProvider),
-                    "@@datastore": "@cloudnux/datastore",
-                    "@@utils": "@cloudnux/utils",
-                }
                 return options;
             },
             bundle: true,
@@ -44,13 +38,12 @@ export const devServerWatch: Task = {
             dts: false,
             watch: false,
             env: {
-                __ENV_PATH__: '"' + path.resolve(__dirname, "../.env").replace(/\\/g, "\\\\") + '"',
+                __ENV_PATH__: '"' + path.resolve(workingDir, "../../.env").replace(/\\/g, "\\\\") + '"',
                 __DEV__: process.env.__DEV__ || '"development"',
             },
             define: {
-                __ENV_PATH__: '"' + path.resolve(__dirname, "../.env").replace(/\\/g, "\\\\") + '"',
+                __ENV_PATH__: '"' + path.resolve(workingDir, "../../.env").replace(/\\/g, "\\\\") + '"',
                 __DEV__: process.env.__DEV__ || '"development"',
-                "process.env.DATABASE_TYPE": '"local"',
             },
             esbuildPlugins: [
                 {
